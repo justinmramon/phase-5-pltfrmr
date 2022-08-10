37fe94fb512d37f239e4ac6d620c89534078cfe0
justinmramon <justinmramon@gmail.com>
2022-08-10 00:03:27 -0500

    configured client-side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch("/hello")
+      .then((r) => r.json())
+      .then((data) => setCount(data.count));
+  }, []);
+
+
   return (
-  <h1 className="text-8xl font-bold underline text-red-600">
-    Hello world!
-  </h1>
+    <div className="testing">
+      <div className="router-test">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/testing" element={<h1>Test Route</h1>} />
+          <Route path="/" element={<h1>Page Count: {count}</h1>} />
+        </Routes>
+      </BrowserRouter>
+      </div>
+      <div className="hello-world">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
+    </div>
   );
 }
 
