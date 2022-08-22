907e0068f17d1f2b917aa393ba02597eff8c0203
justinmramon <justinmramon@gmail.com>
2022-08-21 21:05:25 -0500

    working on playlists

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,33 @@
 class PlaylistsController < ApplicationController
+
+    def index
+        render json: Playlist.all
+    end
+
+    def show
+    end
+
+    def create
+        playlist = Playlist.create!(playlist_params)
+        render json: playlist, status: :created
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+
+    private
+
+    def set_playlist
+      @playlistlist = Playlist.find(params[:id])
+    end
+
+
+    def playlist_params
+      params.permit(:user_id, :game_id)
+    end
+    
 end
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -24,7 +24,6 @@ function NavBar({ user, setUser }) {
             <ul className='flex'>
                 <li className='p-4 bg-white'><Link to="/">Home</Link></li>
                 <li className='p-4 bg-white'>Games</li>
-                <li className='p-4 bg-white'>Lists</li>
                 <li className='p-4 bg-white'>Users</li>
                 <li className='p-4 bg-white cursor-pointer' onClick={ handleLogoutClick }>Logout</li>
             </ul>
